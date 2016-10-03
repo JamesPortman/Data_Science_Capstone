d69cec052051413566e804bacdd42cd3e3118bc1
James Portman <james@portman.ca>
2016-10-03 16:02:49 -0400

    Update server.R

--- ShinyApp/server.R
+++ ShinyApp/server.R
@@ -2,13 +2,21 @@
 # Date: Oct 3, 2016
 # Author: James Portman
 
+# Run-once section
 library(shiny)
-# source('predict.R')
+get(load("ngrams.RData"))
+source('PredictShiny.R', local = TRUE)
+# END Run-once section
 
 shinyServer(function(input, output) {
   
-  df <- data.frame(Next_Word=c("be","do","go","see"), Ranked_Score = c("11","10","7","5"))
-  output$predictedWords <- renderTable(df)
+  # df <- data.frame(Next_Word=c("be","do","go","see"), Ranked_Score = c("11","10","7","5"))
+  # df <- data.frame(Next_Word=c("be"), Ranked_Score = c("11"))
+  # output$predictedWords <- renderTable(df)
+  
+  output$predictedWords <- renderText({
+        predictNextWord(input$text, df_bigrams, df_trigrams, df_fourgrams)
+  })
   
 })
 # End server.R
