5e2232ab1bf4efdc050d659b162a3e8a4c4d4a88
James Portman <james@portman.ca>
2016-10-04 15:32:09 -0400

    Update server.R

--- ShinyApp/server.R
+++ ShinyApp/server.R
@@ -1,5 +1,5 @@
 # server.R
-# Date: Oct 3, 2016
+# Date: Oct 4, 2016
 # Author: James Portman
 
 # Run-once section
@@ -10,13 +10,11 @@ source('PredictShiny.R', local = TRUE)
 
 shinyServer(function(input, output) {
   
-  # df <- data.frame(Next_Word=c("be","do","go","see"), Ranked_Score = c("11","10","7","5"))
-  # df <- data.frame(Next_Word=c("be"), Ranked_Score = c("11"))
-  # output$predictedWords <- renderTable(df)
-  
   output$predictedWords <- renderText({
         predictNextWord(input$text, df_bigrams, df_trigrams, df_fourgrams)
   })
   
 })
 # End server.R
+
+#predictNextWord("I want to", df_bigrams, df_trigrams, df_fourgrams)
