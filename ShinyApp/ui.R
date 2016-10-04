d3871e8c2a296679362828fb312a9edf6d32ba20
James Portman <james@portman.ca>
2016-10-04 15:32:48 -0400

    Update ui.R

--- ShinyApp/ui.R
+++ ShinyApp/ui.R
@@ -1,5 +1,5 @@
 # ui.R
-# Date: Oct 3, 2016
+# Date: Oct 4, 2016
 # Author: James Portman
 
 library(shiny)
@@ -8,19 +8,25 @@ library(shiny)
 shinyUI(fluidPage(
   
   # Application title
-  titlePanel("Word Predictor App"),
-  
+titlePanel("", windowTitle="iClairvoyance"),
+
+img(src = "headerImage.PNG"),
+hr(),
+
   sidebarLayout(
+    
     sidebarPanel(
-      textInput("text", label = h4("Input"), value = "I have to"),
-      helpText("Type a sentence above and then press the 'Predict Next Word' button below. The suggested word will display to the right."),
+     
+      textInput("text", label = h4("Input"), value = ""),
+      helpText("Type a sentence above and press the 'Predict Next Word' button below. The suggested word will display to the right."),
       submitButton("Predict Next Word")
     ),
     
     mainPanel(
-      h4("Next predicted word:", align="left"),
+
+      h4("Predicted word:", align="left"),
       div(tableOutput("predictedWords"), align="left")
     )
   )
-  ))
+))
 # END ui.R
