88a22b32b78acb027d37adfaa2025f45851b054a
James Portman <james@portman.ca>
2016-10-05 11:35:45 -0400

    Add files via upload

---
+++ ShinyApp/server.R
@@ -0,0 +1,20 @@
+# server.R
+# Date: Oct 5, 2016
+# Author: James Portman
+
+# Run-once section
+library(shiny)
+get(load("ngrams.RData"))
+source('PredictShiny.R', local = TRUE)
+# END Run-once section
+
+shinyServer(function(input, output) {
+  
+  output$predictedWords <- renderText({
+        predictNextWord(input$text, df_bigrams, df_trigrams, df_fourgrams)
+  })
+  
+})
+# End server.R
+
+#predictNextWord("I want to", df_bigrams, df_trigrams, df_fourgrams)
